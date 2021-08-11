72bed801d3efa53a9db268cb4a8a6ff3a32104a4
Noubissie237 <noubissiekamgawilfried2017@gmail.com>
2021-08-11 17:56:49 +0200

    fonctions permettant de trier et combiner des tableaux

--- fonctions.php
+++ fonctions.php
@@ -6,44 +6,54 @@
 		  </head>
 	<body>
         <?php
-	// FONCTIONS PERMETTANT D'AJOUTER OU DE SUPPRIMER DES ELEMENTS D'UN TABLEAU
+	// FONCTION PERMETTANT DE TRIER  ET COMBINER DES TABLEAUX
 	
-	// la fonction array_fill permet de remplir un tableau et retourne un autre tableauarra
-	          echo '<pre>';
-			  print_r(array_fill(1,5,'vert')); // tableau commencent a l'indice 1 et retournant 5 fois la valeur 'vert'
-			  echo '<pre>';
-			  
-	// la fonction array_fill_keys permet de remplir un tableau avec des valeur en specifiant des clefs et des elements voulus
-              $clefs = array('a','b','c','d',);
-              print_r ($remplir = array_fill_keys($clefs, 'bleu'));
-	// la fonction array_push permet d'inserer des elements en fin de tableau et la fonction array _pop persmet de supprimer le dernier element d'un tableau
-			  $couleur =array('bleu','vert');
-			  array_push($couleur, 'rouge', 'jaune');
-			  print_r($couleur);
-			  
-			  print_r(array_pop($couleur)); // montre le dernier element qui a ete supprimé
-			   print_r($couleur);
-			   
-	// la fonction array_unshift permet d'ajouter des element au debut d'un tableau et array shift permet de supprimer le premier element d'un tableau
-	        array_unshift($couleur,'gris','belge','gris');
-			print_r($couleur);
-			 
-			echo array_shift($couleur); // affiche la premiere couleur supprimé
-			print_r($couleur);
+	// la fonction array_merge permet de combiner plusieurs tableaux en un nouveau
+	   $alpha = array('a','b');
+	   $beta = array('c', 'd');
+	       $omega=array_merge($alpha,$beta);
+		   echo '<pre>';
+		   print_r($omega);
+   		   echo '<pre>';
+
+    // la fonction array_combine permet de creer un tableaux associatif en combinant 02 tableaux numérotés. NB: les deux tableaux doivent avoir le meme nombre d'elemets. on aura donc que les valeurs du premier tableaux vont servir de clefs au elements du deuxieme tableaux
+		  $lamda=array_combine($alpha,$beta); 
+		  	echo '<pre>';
+            print_r($lamda);
+            echo '</pre>';		
+
+    // la fonction array_unique permet de supprimer les doublonts d'un tableaux conservant ainsi uniquement la premiere valeur NB: il est sensible a la casse		
+		$prenoms = array('4',4,'pierre', 'paul', 'ierre', 'jacque'); // il est a remarquer que dans ce cas, il n'y'a pas de difference entre la chaine de caractere "4" et l'entier 4
+            $redond=array_unique($prenoms);
+            print_r($redond);	
 			
-	// la fonction array_splice permet des supprimer certains elements d'un tableau a partir d'une certaine position et de les remplacer par certain elements. NB: le premier element a pour indice 0.
-	       $couleur1 = array(
-		                        'a' => 'bleu',
-								'b' => 'vert',
-								'c' => 'jaune'
-		   );
-		   $couleur2 = array(
-		                        'a' => 'rouge',
-								'b' => 'violet'
-		   );
-		   array_splice($couleur1,1); // supprime tous les elements du tableau a partir de l'indice 1, c'est a dire du deuxieme jusqu'a l'infini
-		   print_r($couleur1);
-		   
+    // la fonction sort permet d'ordonner les valeurs d'un tableaux numeroté != associatif allant du plus petit au plus grand son contraire est la fonction rsort. NB: il detruit le tableaux initial pour l'ordonner
+	      $eleves = array('pierre', 'antoine', 'jean');
+		  sort($eleves); // classe les elements du tableaux eleve dans l'ordre croissant
+		  print_r($eleves);
+        	
+		  rsort($eleves); // classe les elements du tableaux eleve dans l'ordre decroissant
+          print_r($eleves);		  
+	
+	// la fonctioon ksort permet de classer les elements d'un tableaux associatif dans l'ordre croissant par rapport a ses clefs. sont contraire est krsort
+	 $agenom = array(
+	         'pierre' => 24,
+			 'paul' => 27,
+			 'jean' =>34
+	 );
+	 ksort($agenom); // classe les elements par ordre croissant des clefs
+	 print_r($agenom);
+	 
+	 krsort($agenom); // classe les elements par ordre decroissant des clefs
+	 print_r($agenom);
+	
+	// la fonction asort permet de classer un tableaux associatif selon ses valeurs dans l'ordre croissant, son contraire est arsort;
+	    asort($agenom); // classe les element par ordre croissant des valeurs
+		print_r($agenom);
+		
+		arsort($agenom); // classe les elements par ordre decroissant selon valeurs
+		print_r($agenom);
+	 
  		?>
 
 	</body>
