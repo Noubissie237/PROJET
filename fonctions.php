33a356f2a4c822019a4ad0e91fde53ebd3e715f9
Noubissie237 <noubissiekamgawilfried2017@gmail.com>
2021-08-11 15:30:54 +0200

    fonctions PHP permettant de compter et comparer les tableaux

--- fonctions.php
+++ fonctions.php
@@ -6,60 +6,48 @@
 		  </head>
 	<body>
         <?php
-	// FONCTIONS PHP AFFECTANT LES TABLEAUX
+	// FONCTIONS PERMETTANT DE COMPTER ET COMPARER LES TABLEAUX
 	
-	// la fonction array_keys retourne les clefs d'un tableau dans un nouveau tableau
-	$voitures = array(
-	                      'Citroen' => 'DS3',
-						  'Renault' => 'Clio',
-						  'Peugeot' => '306',
-						  'Peugeot2' => 306);
-						echo '<pre>';
-					print_r(array_keys($voitures));
-					    echo '</pre>';
-								
-					
-    // la fonction array_values retourne les valeurs d'un tableau sans les clefs dans un nouveau tableau	
-    $loisirs = array(
-                          'Sport' => 'Trail',
-						  'Voyage' => 'Ecosse',
-						  'Musique' => 'Guitare'
-	);	echo '<pre>';
-	     print_r(array_values($loisirs));
-		 echo '</pre>';
-		 
-	// la fonction array_key_exists permet de verifier si une clef existe et renvois true si la clef est presente et false dans le cas contraire. NB: il est sensible a la casse
-	$enseignement= array(
-	                      'Math' => 'LOPEZ',
-						  'Info' => 'BICE',
-						  'Physique' => 'TAKEGNI'
-	);
-	   if(array_key_exists('physique',$enseignement)){
-		   echo ' la clef existe'.'<br/>';
-	   }
-	   else{
-		   echo 'la clef n\'existe pas'.'<br/>';
-	   }
-	   
-	   
-	   //la fonction array_search permet de rechercher une valeur dans un tableau et retourner la clef qui lui est associé
-	   if(array_search('LOPEZ' and 'BICE',$enseignement)){
-		   echo 'L\'element rechercher existe'.'<br/>';
-	   }
-	   else{
-		   echo 'L\'element rehercher n\'existe pas'.'<br/>';
-	   };
-	   
-	   //la fonction in_array va rechercher une valeur dans un tableau et renvoyer true si l'element est present et false si l'element est absent
-	   $prenoms= array('pierre', 'paul', 'jacque');
-	   if(in_array('pierre',$prenoms)){
-		   echo 'present'.'<br/>';
-	   }
-	   else{
-		   echo 'absent'.'<br/>';
-	   }
-
-		?>
+	//la fonction count permet de compter le nombre d'elements d'un tableau
+	       $sport=array('football','rugbi','natation');
+		   echo 'Le tableau sport comporte '.count($sport).' elements'.'<br/>';
+	//la fonction array_count_values permet de compter le nombre de fois que l'on a entré chaque valeur du tableau
+            $nombres = array(0,1,2,3,4,5,6,7,8,9,10,18,22,18,25,44,85,2,14,5,22,);
+			echo'<pre>';
+	        print_r(array_count_values($nombres));
+			echo'</pre>';
+			
+	//la fonction array_diff_assoc permet de comparer les clefs et les valeurs de plusieurs tableau et renvoie les differences(ie les clefs et elements qu'il ya dans le premier tableau et absent dans le second tableau  sont renvoyees.NB: les indices doivent exactement correspondre aux valeurs pour pouvoir etre consideré com identique
+	        $compare= array(
+			                  'a' => 'bleu',
+							  'b' => 'vert',
+							  'c' => 'jaune',
+							  'd' => 'rose');
+			$comparant= array(
+			                  'a' => 'jaune',
+							  'b' => 'vert',
+                              'c' => 'bleu'
+			);
+			echo '<pre>';
+					print_r(array_diff_assoc($compare,$comparant));
+			echo '<pre>';
+    // la fonction array_diff permet de la comparaison sur plusieurs tableau uniquement sur les valeur sans tenir compte des indices.
+	       	echo '<pre>';
+					print_r(array_diff($compare,$comparant));
+			echo '<pre>';
+	// la fonction array_intersect_assoc permet de similitudes entre differents tableaux
+				       	echo '<pre>';
+					print_r(array_intersect_assoc($compare,$comparant));
+			           echo '<pre>';
+	// la fonction array_intersect_key permet de comparer les clefs uniquement et renvoyer celle qui sont identiques
+	              		 echo '<pre>';
+					print_r(array_intersect_key($compare,$comparant));
+			           echo '<pre>';
+	// la fonction array_intersect permet de faire la comparaison uniquement sur les valeur
+			       	   echo '<pre>';
+					print_r(array_intersect($compare,$comparant));
+			           echo '<pre>';
+ 		?>
 
 	</body>
  </html>
