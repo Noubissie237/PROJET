d23aca201d628c2e5f8fb9debaf5459b467a2242
Noubissie237 <noubissiekamgawilfried2017@gmail.com>
2021-08-11 17:10:39 +0200

    FONCTIONS PERMETTANT D'AJOUTER OU DE SUPPRIMER DES ELEMENTS D'UN TABLEAU

--- fonctions.php
+++ fonctions.php
@@ -6,47 +6,44 @@
 		  </head>
 	<body>
         <?php
-	// FONCTIONS PERMETTANT DE COMPTER ET COMPARER LES TABLEAUX
+	// FONCTIONS PERMETTANT D'AJOUTER OU DE SUPPRIMER DES ELEMENTS D'UN TABLEAU
 	
-	//la fonction count permet de compter le nombre d'elements d'un tableau
-	       $sport=array('football','rugbi','natation');
-		   echo 'Le tableau sport comporte '.count($sport).' elements'.'<br/>';
-	//la fonction array_count_values permet de compter le nombre de fois que l'on a entré chaque valeur du tableau
-            $nombres = array(0,1,2,3,4,5,6,7,8,9,10,18,22,18,25,44,85,2,14,5,22,);
-			echo'<pre>';
-	        print_r(array_count_values($nombres));
-			echo'</pre>';
+	// la fonction array_fill permet de remplir un tableau et retourne un autre tableauarra
+	          echo '<pre>';
+			  print_r(array_fill(1,5,'vert')); // tableau commencent a l'indice 1 et retournant 5 fois la valeur 'vert'
+			  echo '<pre>';
+			  
+	// la fonction array_fill_keys permet de remplir un tableau avec des valeur en specifiant des clefs et des elements voulus
+              $clefs = array('a','b','c','d',);
+              print_r ($remplir = array_fill_keys($clefs, 'bleu'));
+	// la fonction array_push permet d'inserer des elements en fin de tableau et la fonction array _pop persmet de supprimer le dernier element d'un tableau
+			  $couleur =array('bleu','vert');
+			  array_push($couleur, 'rouge', 'jaune');
+			  print_r($couleur);
+			  
+			  print_r(array_pop($couleur)); // montre le dernier element qui a ete supprimé
+			   print_r($couleur);
+			   
+	// la fonction array_unshift permet d'ajouter des element au debut d'un tableau et array shift permet de supprimer le premier element d'un tableau
+	        array_unshift($couleur,'gris','belge','gris');
+			print_r($couleur);
+			 
+			echo array_shift($couleur); // affiche la premiere couleur supprimé
+			print_r($couleur);
 			
-	//la fonction array_diff_assoc permet de comparer les clefs et les valeurs de plusieurs tableau et renvoie les differences(ie les clefs et elements qu'il ya dans le premier tableau et absent dans le second tableau  sont renvoyees.NB: les indices doivent exactement correspondre aux valeurs pour pouvoir etre consideré com identique
-	        $compare= array(
-			                  'a' => 'bleu',
-							  'b' => 'vert',
-							  'c' => 'jaune',
-							  'd' => 'rose');
-			$comparant= array(
-			                  'a' => 'jaune',
-							  'b' => 'vert',
-                              'c' => 'bleu'
-			);
-			echo '<pre>';
-					print_r(array_diff_assoc($compare,$comparant));
-			echo '<pre>';
-    // la fonction array_diff permet de la comparaison sur plusieurs tableau uniquement sur les valeur sans tenir compte des indices.
-	       	echo '<pre>';
-					print_r(array_diff($compare,$comparant));
-			echo '<pre>';
-	// la fonction array_intersect_assoc permet de similitudes entre differents tableaux
-				       	echo '<pre>';
-					print_r(array_intersect_assoc($compare,$comparant));
-			           echo '<pre>';
-	// la fonction array_intersect_key permet de comparer les clefs uniquement et renvoyer celle qui sont identiques
-	              		 echo '<pre>';
-					print_r(array_intersect_key($compare,$comparant));
-			           echo '<pre>';
-	// la fonction array_intersect permet de faire la comparaison uniquement sur les valeur
-			       	   echo '<pre>';
-					print_r(array_intersect($compare,$comparant));
-			           echo '<pre>';
+	// la fonction array_splice permet des supprimer certains elements d'un tableau a partir d'une certaine position et de les remplacer par certain elements. NB: le premier element a pour indice 0.
+	       $couleur1 = array(
+		                        'a' => 'bleu',
+								'b' => 'vert',
+								'c' => 'jaune'
+		   );
+		   $couleur2 = array(
+		                        'a' => 'rouge',
+								'b' => 'violet'
+		   );
+		   array_splice($couleur1,1); // supprime tous les elements du tableau a partir de l'indice 1, c'est a dire du deuxieme jusqu'a l'infini
+		   print_r($couleur1);
+		   
  		?>
 
 	</body>
