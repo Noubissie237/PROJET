5f93bf2c3d9411350fdf6d1066dad32ce52aff27
Noubissie237 <noubissiekamgawilfried2017@gmail.com>
2021-08-12 15:54:43 +0200

    Fonctions permettant de formater des dates

--- fonctions.php
+++ fonctions.php
@@ -6,54 +6,101 @@
 		  </head>
 	<body>
         <?php
-	// FONCTION PERMETTANT DE TRIER  ET COMBINER DES TABLEAUX
+    // FONCTIONS PERMETTANT DE FORMATER DES DATES
 	
-	// la fonction array_merge permet de combiner plusieurs tableaux en un nouveau
-	   $alpha = array('a','b');
-	   $beta = array('c', 'd');
-	       $omega=array_merge($alpha,$beta);
-		   echo '<pre>';
-		   print_r($omega);
-   		   echo '<pre>';
+	/*  d: represente un jour du mois( de 01 a 31)
+	    m: represente un mois(de 01 a 12)
+	    n: represente un mois(de 1 a 12)
+		Y(y_majuscule): represente l'annee( par exemple: 2021)
+		l: represente le jour de la semaine( ex: jeudi)
+		N: represente un jour de la semaine, en chiffre(ex: jeudi = 4)
+		H: represente les heure(de 00 a 23) format 24h
+		h: represente les heures (de 00 a 12) format 12h
+		i represente les minutes( de 00 a 59)
+		s represente les secondes( de 00 a 59)
+		NB: la casse a un grand role a jouer
+	*/
+	 echo date ('d/m/Y').'<br/>'; // affiche la date le mois et l'année
+	 /* exemple d'affichage complet*/
+	     echo 'Nous sommes le '.date('d-m-Y', ).'<br/>';
+		 echo 'Aujourd\'hui c\'est '.date('l').'<br/>';
+		 echo 'Il est '.date('H:i:s').'<br/>';
+	
+        $jour = array(
+                        '',
+                        'Lundi',
+                        'Mardi',
+                        'Mercredi',
+						'Jeudi',
+						'Vendredi',
+						'Samedi',
+						'Dimance');
+		$mois = array(
+                        '',
+                        'Janvier',
+						'Fevrier',
+						'Mars',
+						'Avril',
+						'Mai',
+						'Juin',
+						'Juillet',
+						'Aout',
+						'Septembre',
+						'Octobre',
+						'Novembre',
+						'Decembre'				
+		);
+                  echo '<br/><br/><br/><br/><br/><br/><br/><br/>';
+				  echo 'Pour la date du jour en Francais, nous avons : <br/> Aujourd\'hui c\'est '.$jour[date('N')]; 
+				  echo '<br/><br/><br/><br/><br/><br/><br/><br/>';
+				  echo nl2br('Pour le mois de l\'année en Francais, nous avous: 
+				             Nous somme en '.$mois[date('n')]);
+							 
+				   echo '<br/><br/><br/><br/><br/><br/><br/><br/>';
+				   echo 'Nous sommes le '.$jour[date('N')].', '.date('d ').$mois[date('n')].' '.date('Y').'<br/>';
+				   
+	// AUTRE FACON D'AFFICHER
+	
+	               setlocale(LC_TIME, 'fr_FR');
+				   echo 'Nous sommes le '.strftime('%A %d %B %Y').'<br/>'; // qui ne fonctionne pas et je ne sais pas encore pour quoi
 
-    // la fonction array_combine permet de creer un tableaux associatif en combinant 02 tableaux numérotés. NB: les deux tableaux doivent avoir le meme nombre d'elemets. on aura donc que les valeurs du premier tableaux vont servir de clefs au elements du deuxieme tableaux
-		  $lamda=array_combine($alpha,$beta); 
-		  	echo '<pre>';
-            print_r($lamda);
-            echo '</pre>';		
+    // la fonction checkdate permet de tester si une date est valide ou pas		
+				   $date1 = checkdate(12,31,2021); // le premier argument c'est le mois, le deuxieme c'est le jour et le troisieme c'est l'année
+				   $date2 = checkdate(13,31,2021);
+				   $date3 = checkdate(01,25,-2021);
+				  	
+				   if($date1){
+					   echo ' La date est valide ! <br/>';
+				   }
+				   else{
+				       echo 'La date n\'est pas valide <br/>';
+				   }	
+                   
+                   if($date2){
+					   echo'la date est valide ! <br/>';
+				   }
+				   else{
+				       echo 'La date n\'est pas valide <br/>';
+				   }	
+                   if($date3){
+					   echo'la date est valide ! <br/>';
+				   }
+				   else{
+				       echo 'La date n\'est pas valide <br/>';
+				   }				    
+				 
+	// la fonction getdate permet de d'afficher la toutes les informations consernant la dates.
+	                    echo'<pre>';
+	                 print_r(getdate());
+					    echo'</pre>'.'<br/><br/>';
 
-    // la fonction array_unique permet de supprimer les doublonts d'un tableaux conservant ainsi uniquement la premiere valeur NB: il est sensible a la casse		
-		$prenoms = array('4',4,'pierre', 'paul', 'ierre', 'jacque'); // il est a remarquer que dans ce cas, il n'y'a pas de difference entre la chaine de caractere "4" et l'entier 4
-            $redond=array_unique($prenoms);
-            print_r($redond);	
-			
-    // la fonction sort permet d'ordonner les valeurs d'un tableaux numeroté != associatif allant du plus petit au plus grand son contraire est la fonction rsort. NB: il detruit le tableaux initial pour l'ordonner
-	      $eleves = array('pierre', 'antoine', 'jean');
-		  sort($eleves); // classe les elements du tableaux eleve dans l'ordre croissant
-		  print_r($eleves);
-        	
-		  rsort($eleves); // classe les elements du tableaux eleve dans l'ordre decroissant
-          print_r($eleves);		  
-	
-	// la fonctioon ksort permet de classer les elements d'un tableaux associatif dans l'ordre croissant par rapport a ses clefs. sont contraire est krsort
-	 $agenom = array(
-	         'pierre' => 24,
-			 'paul' => 27,
-			 'jean' =>34
-	 );
-	 ksort($agenom); // classe les elements par ordre croissant des clefs
-	 print_r($agenom);
-	 
-	 krsort($agenom); // classe les elements par ordre decroissant des clefs
-	 print_r($agenom);
-	
-	// la fonction asort permet de classer un tableaux associatif selon ses valeurs dans l'ordre croissant, son contraire est arsort;
-	    asort($agenom); // classe les element par ordre croissant des valeurs
-		print_r($agenom);
-		
-		arsort($agenom); // classe les elements par ordre decroissant selon valeurs
-		print_r($agenom);
-	 
+	                    echo'<pre>';
+	                 print_r(getdate(1));
+					    echo'</pre>';
+
+							 
+                  						
+		 
  		?>
 
 	</body>
